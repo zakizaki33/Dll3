a2b95c10ce07ee21c3ab2866a1a357ddcdcbf2e3
zakizaki33 <zakizaki33@hotmail.com>
2022-11-17 16:52:10 +0900

    ダブルポインタを理解進める

--- Dll3/testfunction.h
+++ Dll3/testfunction.h
@@ -17,7 +17,7 @@ struct complex_wrap {
 struct matrix_wrap {
 	int m = 0;
 	int n = 0;
-	// double** dpmatrix_wrap;
+	double** dpmatrix_wrap;
 };
 
 // 単なるテスト関数
@@ -36,8 +36,21 @@ extern "C" MATHLIBRARY_API double ARG(complex_wrap*);
 extern "C" MATHLIBRARY_API void Conjugate(complex_wrap*, complex_wrap*);
 
 // ここからはmatrixに関する関数
+// newの時に、ポインタが必要。なので引数が1つ増える
+// 
 extern "C" MATHLIBRARY_API void New_matrix();
+extern "C" MATHLIBRARY_API void New_matrix(matrix_wrap* p);
+extern "C" MATHLIBRARY_API void New_matrix(matrix_wrap* p, int m, int n);
+
+// matrix(const matrix<T>&); ??オリジナル
+// 候補2
+extern "C" MATHLIBRARY_API void New_matrix(matrix_wrap* p1, const matrix_wrap& );
+// 候補1
+extern "C" MATHLIBRARY_API void New_matrix(matrix_wrap* p1, const matrix_wrap* p2);
+
+
 
 extern "C" MATHLIBRARY_API void Delete_matrix();
 
+// ↓これは要らない
 extern "C" MATHLIBRARY_API void matrix_init(matrix_wrap * p, int m, int n);
